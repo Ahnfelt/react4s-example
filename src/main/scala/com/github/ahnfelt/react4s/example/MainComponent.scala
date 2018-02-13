ce16456e5e9418c0ee6e8abacd3a4b5faac9e7c6
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2018-02-13 23:15:23 +0100

    Now using own server in front of Spotify (since they closed the open access)

--- src/main/scala/com/github/ahnfelt/react4s/example/MainComponent.scala
+++ src/main/scala/com/github/ahnfelt/react4s/example/MainComponent.scala
@@ -14,7 +14,7 @@ case class MainComponent() extends Component[NoEmit] {
 
     val artists = Loader(this, debouncedQuery) { q =>
         if(q.trim.isEmpty) Future.successful(List()) else {
-            Ajax.get("https://api.spotify.com/v1/search?q=" + js.URIUtils.encodeURIComponent(q) + "&type=artist").
+            Ajax.get("http://show.ahnfelt.net/react4s-spotify/?q=" + js.URIUtils.encodeURIComponent(q) + "&type=artist").
                 map { ajax =>
                     js.JSON.parse(ajax.responseText).
                         artists.items.
